378c2d1ae05c2ff091725b049bfa1a4cfc32ec8a
PushparajNt <pushparajcsegce@gmail.com>
2022-04-11 07:10:49 +0530

    Added other models to the project

---
+++ src/main/java/com/pushparaj/redditclone/model/VerificationToken.java
@@ -0,0 +1,32 @@
+package com.pushparaj.redditclone.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "token")
+public class VerificationToken {
+
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private Long id;
+	private String token;
+	@OneToOne( fetch = FetchType.LAZY)
+	private String user;
+	private String expiryToken;
+	
+}
